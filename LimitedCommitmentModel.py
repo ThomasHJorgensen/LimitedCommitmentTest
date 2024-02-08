562fd69508aa2dc924e82348f822cfcee437cff4
Malene Laczek <mcf@econ.ku.dk>
2024-02-08 10:15:41 +0100

    update initi_distr

--- LimitedCommitmentModel.py
+++ LimitedCommitmentModel.py
@@ -71,7 +71,8 @@ class LimitedCommitmentModelClass(EconModelClass):
         
         # wealth
         par.num_A = 50
-        par.max_A = 30.0
+        par.num_A = 20
+        par.max_A = 15.0
         #par.max_A = 3000.0
         par.max_Aw = par.max_A*par.div_A_share 
         par.max_Am = par.max_A*(1-par.div_A_share )
@@ -85,7 +86,8 @@ class LimitedCommitmentModelClass(EconModelClass):
         par.num_shock_K = 5
         
         # bargaining power
-        par.num_power = 21
+        #par.num_power = 21
+        par.num_power = 11
 
         # love/match quality
         par.num_love = 10
@@ -94,6 +96,12 @@ class LimitedCommitmentModelClass(EconModelClass):
         par.sigma_love = 0.031  # from Mariage labor supply and the Dynamics of social safety net
         #par.sigma_love = 0.0001
         par.num_shock_love = 5 
+
+        #divorce quality
+        par.pr_z = 0.05
+
+        #initial distribution factor
+        par.pr_distr_factor = 0.5
 
         # simulation
         par.seed = 9210
@@ -123,6 +131,7 @@ class LimitedCommitmentModelClass(EconModelClass):
         
         # singles
         shape_single = (par.T,par.num_A,par.num_K)                        # single states: T and assets
+        #shape_single = (par.T,par.num_divorce_shock, par.num_divorce_shock,par.num_A,par.num_K)                        # single states: T and assets
         sol.Vw_single = np.nan + np.ones(shape_single)
         sol.Vm_single = np.nan + np.ones(shape_single)      
         sol.labor_w_single = np.nan + np.ones(shape_single)     # labor supply, single women
@@ -139,6 +148,7 @@ class LimitedCommitmentModelClass(EconModelClass):
        
         # couples
         shape_couple = (par.T,par.num_power,par.num_love,par.num_A,par.num_K,par.num_K)     # states when couple: T, assets, power, love
+        #shape_couple = (par.T,par.num_divorce_shock, par.num_divorce_shock,par.num_power,par.num_love,par.num_A,par.num_K,par.num_K)     # states when couple: T, assets, power, love
         sol.Vw_couple = np.nan + np.ones(shape_couple) # value of starting as couple
         sol.Vm_couple = np.nan + np.ones(shape_couple)
         sol.labor_w_couple = np.nan + np.ones(shape_couple)
@@ -158,7 +168,8 @@ class LimitedCommitmentModelClass(EconModelClass):
         sol.power = np.zeros(shape_couple)   
 
         # post-decision pre-computation
-        shape_pd = (par.num_power,par.num_love,par.num_A_pd,par.num_K_pd,par.num_K_pd)     
+        shape_pd = (par.num_power,par.num_love,par.num_A_pd,par.num_K_pd,par.num_K_pd)    
+        #shape_pd = (par.num_divorce_shock, par.num_divorce_shock, par.num_power,par.num_love,par.num_A_pd,par.num_K_pd,par.num_K_pd)     
         sol.EVw_pd = np.nan + np.ones(shape_pd)
         sol.EVm_pd = np.nan + np.ones(shape_pd)                           
 
@@ -179,6 +190,8 @@ class LimitedCommitmentModelClass(EconModelClass):
         sim.love = np.nan + np.ones(shape_sim)
         sim.Kw = np.nan + np.ones(shape_sim)
         sim.Km = np.nan + np.ones(shape_sim)
+        sim.Zw = np.nan + np.ones(shape_sim)
+        sim.Zm = np.nan + np.ones(shape_sim)
         sim.value = np.nan + np.ones(shape_sim)
         sim.util = np.nan + np.ones(shape_sim)
 
@@ -187,8 +200,10 @@ class LimitedCommitmentModelClass(EconModelClass):
         sim.draw_love = par.sigma_love * np.random.normal(size=shape_sim)
         sim.draw_Kw = np.exp(-0.5*par.sigma_K**2 + par.sigma_K*np.random.normal(size=shape_sim))
         sim.draw_Km = np.exp(-0.5*par.sigma_K**2 + par.sigma_K*np.random.normal(size=shape_sim))
-        sim.draw_Kw = par.sigma_K*np.random.normal(size=shape_sim)
-        sim.draw_Km = par.sigma_K*np.random.normal(size=shape_sim)
+        #sim.draw_Kw = par.sigma_K*np.random.normal(size=shape_sim)
+        #sim.draw_Km = par.sigma_K*np.random.normal(size=shape_sim)
+        sim.draw_Zw = np.random.uniform(size=shape_sim)
+        sim.draw_Zm = np.random.uniform(size=shape_sim) 
 
         # initial distribution
         sim.init_A = par.grid_A[0] + np.zeros(par.simN)
@@ -199,10 +214,14 @@ class LimitedCommitmentModelClass(EconModelClass):
         sim.init_love = np.zeros(par.simN)
         #TODO: TÆNK OVER DENNE MED INIT, lige nu stor variation for at få variation i initiale barganing
         # det giver en periode med meget højt løn (uden HK) generelt højere løn (med HK). 
-        sim.init_Kw = np.exp(-0.5*par.sigma_K_init**2 + par.sigma_K_init*np.random.normal(size=par.simN))
-        sim.init_Km = np.exp(-0.5*par.sigma_K_init**2 + par.sigma_K_init*np.random.normal(size=par.simN))
-        #sim.init_Kw = np.random.uniform(low=0.0,high = 2.0,size=par.simN)
-        #sim.init_Km = np.random.uniform(low=0.0,high = 2.0,size=par.simN)
+        #sim.init_Kw = np.exp(-0.5*par.sigma_K_init**2 + par.sigma_K_init*np.random.normal(size=par.simN))
+        #sim.init_Km = np.exp(-0.5*par.sigma_K_init**2 + par.sigma_K_init*np.random.normal(size=par.simN))
+        sim.init_Kw = np.random.uniform(low=0.0,high = 2.0,size=par.simN)
+        sim.init_Km = np.random.uniform(low=0.0,high = 2.0,size=par.simN)
+        sim.init_Zw = np.zeros(par.simN)
+        sim.init_Zm = np.zeros(par.simN)
+
+        sim.init_distr = np.random.choice(3,par.simN, p =[par.pr_distr_factor*(1-par.pr_distr_factor),par.pr_distr_factor*par.pr_distr_factor+(1-par.pr_distr_factor)*(1-par.pr_distr_factor),par.pr_distr_factor*(1-par.pr_distr_factor)])
 
         
         
@@ -244,6 +263,16 @@ class LimitedCommitmentModelClass(EconModelClass):
 
         else:
             par.grid_shock_K,par.grid_weight_K = quadrature.log_normal_gauss_hermite(par.sigma_K,par.num_shock_K)
+
+        #divorce utility grid
+        par.num_divorce_shock = 2 #high or low bmi
+        par.grid_Z = np.array([0,1])
+        par.pr_zplus = np.array([[1-par.pr_z,par.pr_z],[par.pr_z, 1-par.pr_z]])
+
+        #initially distrbution factor
+        par.num_distr_factor = 3 #high or low bmi
+        par.grid_distr_factor = np.array([0,1,2]) #woman highest, equal, woman higest
+        par.pr_distr_factor = np.array([[par.pr_distr_factor*(1-par.pr_distr_factor),par.pr_distr_factor*par.pr_distr_factor+(1-par.pr_distr_factor)*(1-par.pr_distr_factor),par.pr_distr_factor*(1-par.pr_distr_factor)]])
 
         # post-decision states
         par.grid_A_pd = nonlinspace(0.0,par.max_A,par.num_A_pd,1.1)       # asset grid
