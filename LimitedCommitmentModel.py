8901499feb0bfff6301df0c922560a62842bb5de
MaleneFuglsang <61835257+MaleneFuglsang@users.noreply.github.com>
2024-05-21 09:18:26 +0200

    update figure

--- LimitedCommitmentModel.py
+++ LimitedCommitmentModel.py
@@ -107,7 +107,7 @@ class LimitedCommitmentModelClass(EconModelClass):
         #divorce quality
         par.pr_z = 0.05
         #par.util_Z = -0.5
-        par.util_Z = -0.1
+        par.util_Z = -0.25
 
         #initial distribution factor
         par.pr_distr_factor = 0.5
@@ -237,10 +237,10 @@ class LimitedCommitmentModelClass(EconModelClass):
         sim.init_Km = np.random.uniform(low=0.0, high =0.2,size=shape_sim)
         #sim.init_Kw = np.exp(-0.5*par.sigma_K_init**2 + par.sigma_K_init*np.random.normal(size=par.simN))
         #sim.init_Km = np.exp(-0.5*par.sigma_K_init**2 + par.sigma_K_init*np.random.normal(size=par.simN))
-        #sim.init_Zw = np.zeros(par.simN)
-        #sim.init_Zm = np.zeros(par.simN)
-        sim.init_Zw = np.random.choice(2,par.simN)
-        sim.init_Zm = np.random.choice(2,par.simN)
+        sim.init_Zw = np.zeros(par.simN)
+        sim.init_Zm = np.zeros(par.simN)
+        #sim.init_Zw = np.random.choice(2,par.simN)
+        #sim.init_Zm = np.random.choice(2,par.simN)
 
 
         sim.init_distr = np.random.choice(3,par.simN, p =[par.pr_distr_factor*(1-par.pr_distr_factor),par.pr_distr_factor*par.pr_distr_factor+(1-par.pr_distr_factor)*(1-par.pr_distr_factor),par.pr_distr_factor*(1-par.pr_distr_factor)])
